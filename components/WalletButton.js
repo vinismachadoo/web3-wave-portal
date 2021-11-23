67f9f6365cea45455977375b892a4653c5d14413
vinismachadoo <vinicius.sanches@poli.ufrj.br>
2021-11-22 21:51:12 -0300

    logic based on components states

--- components/WalletButton.js
+++ components/WalletButton.js
@@ -21,30 +21,31 @@ const WalletButton = ({
     <div>
       {!walletInstalled && (
         <a href="https://metamask.io/download.html" target="_blank">
-          <button className="px-3 py-0.5 text-sm md:text-lg font-semibold rounded-lg bg-black">
+          <button className="px-3 py-0.5 text-sm md:text-lg rounded-lg border-2 border-purple-500 bg-purple-500 hover:bg-transparent hover:text-purple-500">
             Install MetaMask ⬇️
           </button>
         </a>
       )}
       {walletInstalled && !walletConnected && (
         <button
-          className="px-3 py-0.5 text-sm md:text-lg font-semibold rounded-lg bg-gradient-to-br from-purple-500 to-pink-500"
-          onClick={() => connectWallet}
+          className="px-3 py-0.5 text-sm md:text-lg rounded-lg border-2 border-purple-500 bg-purple-500 hover:bg-transparent hover:text-purple-500"
+          onClick={() => connectWallet()}
         >
           Connect Wallet 🦊
         </button>
       )}
       {walletConnected && (
         <button
+          disabled={!message || waveLoading !== TRANSACTION_STATUS.None}
           className={classNames(
             message && waveLoading === TRANSACTION_STATUS.None
-              ? "hover:bg-transparent hover:text-purple-500"
+              ? "hover:bg-transparent hover:border-black hover:text-black"
               : "opacity-50 cursor-not-allowed",
             "px-3 py-0.5 text-sm md:text-lg rounded-lg border-2 border-purple-500 bg-purple-500"
           )}
           onClick={() => sendWave(message)}
         >
-          Wave at me 👋
+          Wave at me 👋 {walletConnected}
         </button>
       )}
     </div>
