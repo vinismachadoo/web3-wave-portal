8d813da1d7d1875bd675260d8106e98a0531a59f
vinismachadoo <vinicius.sanches@poli.ufrj.br>
2022-01-04 20:41:51 -0300

    emojis to png

--- components/WaveList.js
+++ components/WaveList.js
@@ -8,22 +8,28 @@ const WaveList = ({ waveList }) => {
 
         return (
           <div
-            className="my-2 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 bg-white rounded-xl p-3"
+            className="my-2 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50 z-0 bg-white rounded-xl p-3"
             key={index}
           >
             <div className="flex">
-              <div className="flex text-3xl mx-3 items-center">👋</div>
-              <div>
-                <div className="text-xs md:text-sm">
-                  <span className="mr-2 font-bold">From:</span>
+              <div className="hidden sm:flex items-center mx-3 z-30">
+                <img
+                  src="/wave.png"
+                  alt="wave"
+                  className="w-4 sm:w-8 h-4 sm:h-8 mx-auto opacity-100"
+                />
+              </div>
+              <div className="w-full">
+                <div className="hidden sm:flex text-xs md:text-sm before:content-['From:'] before:font-bold before:mr-1">
                   {waver}
                 </div>
-                <div className="text-xs md:text-sm">
-                  <span className="mr-2 font-bold">Time:</span>
+                <div className="flex sm:hidden text-xs md:text-sm before:content-['From:'] before:font-bold before:mr-1">
+                  ...{waver.slice(waver.length - 8)}
+                </div>
+                <div className="text-xs md:text-sm before:content-['Time:'] before:font-bold before:mr-1 line-clamp-1">
                   {timestamp}
                 </div>
-                <div className="text-xs md:text-sm">
-                  <span className="mr-2 font-bold">Message:</span>
+                <div className="text-xs md:text-sm before:content-['Message:'] before:font-bold before:mr-1 line-clamp-3 break-words">
                   {message}
                 </div>
               </div>
